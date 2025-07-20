dd2762d7a861d84ba009e1d507b58d270037197e
kevin-geor-ge <kevingeorgejimon2028@cs.sjcetpalai.ac.in>
2025-07-20 15:51:57 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class sum {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        System.out.println("Enter first number");
+        int a =in.nextInt();
+        System.out.println("Enter second number");
+        int b =in.nextInt();
+        System.out.println("The sum is "+(a+b));
+    }
+}
