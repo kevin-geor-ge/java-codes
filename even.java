ba8468e68beb5814812a5249359b608dd938436f
kevin-geor-ge <kevingeorgejimon2028@cs.sjcetpalai.ac.in>
2025-07-20 15:45:55 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,13 @@
+/**Author: KevinGeorge Jimon
+ * Date:19-07-2025
+ * Write a Java program to print all even numbers between 1 and 20 using a do while looр.
+ */
+public class Even {
+    public static void main(String[] args) {
+        int i=2;
+        do{
+            System.out.print(i+" ");
+            i+=2;
+        }while(i<=20);
+    }
+}
